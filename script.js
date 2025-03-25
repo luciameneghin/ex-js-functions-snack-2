4dbe200a9e5c06ef49bfd3b81740a13c57db51d5
luciameneghin <lucia.meneghin00@gmail.com>
2025-03-25 18:37:09 +0100

    fea: snack-3

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ console.log(sommaFreccia(10, 7));
 //snack 2
 const quadrato = (n) => n * n;
 console.log(quadrato(4));
+
+
+//snack 3
+function eseguiOperazione(x, y, operazione) {
+  return operazione(x, y);
+}
+
+const somma = (x, y) => x + y;
+console.log(eseguiOperazione(2, 3, somma));
+
+const moltiplica = (x, y) => x * y;
+console.log(eseguiOperazione(2, 3, moltiplica));
+
+const sottrai = (x, y) => x - y;
+console.log(eseguiOperazione(10, 3, sottrai));
